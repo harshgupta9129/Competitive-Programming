014df6839db795dafa3ebbcd5ac0214b9a37a247
Harsh Gupta <rockstar221405@gmail.com>
2026-08-01 12:48:01 +0530

    Time: 78 ms (71.24%) | Memory: 56 MB (72.23%) - LeetSync

--- LeetCode/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ LeetCode/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Exactly(k) = AtMost(k) - AtMost(k-1)
+    // because counting at most K distinct is easy with sliding window
     int atmost(vector <int> &nums, int k) {
         int n = nums.size();
         int start = 0;
@@ -8,9 +10,8 @@ public:
         for (int end=0; end<n; end++) {
             mp[nums[end]]++;
             while (mp.size()>k) {
-                if (--mp[nums[start]]==0) {
-                    mp.erase(nums[start]);
-                }
+                mp[nums[start]]--;
+                if (mp[nums[start]]==0) mp.erase(nums[start]);
                 start++;
             }
             ans+=(end-start+1);
