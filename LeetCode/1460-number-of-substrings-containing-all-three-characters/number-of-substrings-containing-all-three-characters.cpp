2c0b1d0e725bce869d4a15da84a9df2d70932be2
Harsh Gupta <rockstar221405@gmail.com>
2026-08-02 22:12:02 +0530

    Time: 7 ms (63.79%) | Memory: 11.1 MB (52.13%) - LeetSync

--- LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    int atmost (string s, int k) {
+    int numberOfSubstrings(string s) {
         int n = s.length();
+        int i = 0;
         int ans = 0;
-        int i=0;
-        unordered_map <char, int> mp;
+        int countA = 0, countB = 0, countC = 0;
         for (int j=0; j<n; j++) {
-            mp[s[j]]++;
-            while (mp.size()>k) {
-                if (--mp[s[i]]==0) mp.erase(s[i]);
+            if (s[j]=='a') countA++;
+            else if (s[j]=='b') countB++;
+            else countC++;
+            while (countA>0 && countB>0 && countC>0) {
+                ans+=(n-j);
+                if (s[i]=='a') countA--;
+                else if (s[i]=='b') countB--;
+                else countC--;
                 i++;
             }
-            ans+=(j-i+1);
         }
         return ans;
     }
-    int numberOfSubstrings(string s) {
-        return atmost(s, 3) - atmost(s, 2);
-    }
+
+    // At Most Approach
+    // int atmost (string s, int k) {
+    //     int n = s.length();
+    //     int ans = 0;
+    //     int i=0;
+    //     unordered_map <char, int> mp;
+    //     for (int j=0; j<n; j++) {
+    //         mp[s[j]]++;
+    //         while (mp.size()>k) {
+    //             if (--mp[s[i]]==0) mp.erase(s[i]);
+    //             i++;
+    //         }
+    //         ans+=(j-i+1);
+    //     }
+    //     return ans;
+    // }
+    // int numberOfSubstrings(string s) {
+    //     return atmost(s, 3) - atmost(s, 2);
+    // }
 };
