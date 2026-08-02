b80697fef615405dfc7bf46e1645dfc7e4af2704
Harsh Gupta <rockstar221405@gmail.com>
2026-08-02 18:30:28 +0530

    Time: 43 ms (59.70%) | Memory: 84.9 MB (34.40%) - LeetSync

--- LeetCode/940-fruit-into-baskets/fruit-into-baskets.cpp
+++ LeetCode/940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        // count of fruits in current window
         unordered_map <int, int> mp;
         int ans = 0;
         int n = fruits.size();
         int start = 0;
         for (int end = 0; end<n; end++) {
             mp[fruits[end]]++;
+            // Shrink window if more than 2 types
             while (mp.size()>2) {
                 if (--mp[fruits[start]]==0) mp.erase(fruits[start]);
                 start++;
             }
+            // Update maximum fruits collected
             ans = max(ans, end-start+1);
         }
         return ans;
