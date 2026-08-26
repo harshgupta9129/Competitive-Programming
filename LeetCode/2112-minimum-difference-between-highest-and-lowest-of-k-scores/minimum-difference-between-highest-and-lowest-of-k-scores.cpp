9c53b14626138a72502ebf997baa127049bc7a3b
Harsh Gupta <rockstar221405@gmail.com>
2026-08-26 21:03:28 +0530

    Time: 2 ms (47.61%) | Memory: 17.2 MB (91.76%) - LeetSync

--- LeetCode/2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ LeetCode/2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -3,17 +3,9 @@ public:
     int minimumDifference(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        map <int, int> mp;
-        int i=0;
         int ans = INT_MAX;
-        for (int j=0; j<n; j++) {
-            mp[nums[j]]++;
-            while (j-i+1 > k) {
-                mp[nums[i]]--;
-                if (mp[nums[i]]==0) mp.erase(nums[i]);
-                i++;
-            }
-            if (j-i+1==k) ans = min(ans, mp.rbegin()->first - mp.begin()->first);
+        for (int i=k-1; i<n; i++) {
+            ans = min(ans, nums[i] - nums[i-(k-1)]);
         }
         return ans;
     }
