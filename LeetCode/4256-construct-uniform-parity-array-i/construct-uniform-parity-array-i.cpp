201c01c8e8992b936ff03ce0c8bf01cce365ea76
Harsh Gupta <rockstar221405@gmail.com>
2026-09-02 23:04:30 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (67.80%) - LeetSync

---
+++ LeetCode/4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // Suppose All are even or All are Odd Ok
+        // But what if mix then we know that difference of odd and even is always odd so we can always create a all odd elements array
+        return true;
+    }
+};
