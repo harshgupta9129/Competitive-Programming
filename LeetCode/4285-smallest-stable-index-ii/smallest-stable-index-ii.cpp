bff9201ac83601053834b77ba8d34d63f06c831b
Harsh Gupta <rockstar221405@gmail.com>
2026-09-06 00:37:28 +0530

    Time: 6 ms (80.60%) | Memory: 202.4 MB (96.72%) - LeetSync

--- LeetCode/4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ LeetCode/4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        map <int, int> mpmini, mpmaxi;
-        for (int x : nums) mpmini[x]++;
+        vector <int> mini(n, 0);
+        mini[n-1] = nums[n-1];
+        for (int i=n-2; i>=0; i--) mini[i] = min(nums[i], mini[i+1]);
+        int maxi = nums[0];
         for (int i=0; i<n; i++) {
-            mpmaxi[nums[i]]++;
-            if ((mpmaxi.rbegin()->first - mpmini.begin()->first) <= k) return i;
-            if (--mpmini[nums[i]] == 0) mpmini.erase(nums[i]);
+            maxi = max(maxi, nums[i]);
+            if (maxi-mini[i] <= k) return i;
         }
         return -1;
     }
